af46fa3b1c22c1da671e9f27011df6c9de9f094f
Valdir Silva <valdir.ti@gmail.com>
2020-12-29 11:38:49 -0300

    feat: SignUp screen and navigation

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 165 : 40}px;
 `;
 
 export const Title = styled.Text`
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,79 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container>
-      <Text>SignUp</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input
+              name="Name"
+              icon="user"
+              placeholder="Nome"
+              keyboardType="default"
+            />
+
+            <Input
+              name="Email"
+              icon="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+            />
+
+            <Input
+              name="Password"
+              icon="lock"
+              placeholder="Senha"
+              keyboardType="numeric"
+            />
+
+            <Button
+              onPress={() => {
+                console.log('SignIn');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={18} color="#fff" />
+        <BackToSignInText>Voltar para o logon</BackToSignInText>
+      </BackToSignIn>
+    </>
   );
 };
 
